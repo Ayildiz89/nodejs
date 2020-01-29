6897eeba6b51a956463990b9fe56eedfcdfe33c2
Emre Ozbay <eozbayirtibat@yahoo.com>
2020-01-29 11:14:35 +0100

    db local icin ayarlanmali

--- app/database.js
+++ app/database.js
@@ -4,7 +4,9 @@ var db = {}
 
 const sequelize = new Sequelize('flow', 'root', 'Aa20012001', {
     host: '212.227.192.142',
+    //host: '127.0.0.1',
     port: '3306',
+    //port: '8889',
     dialect: 'mysql',
     define: {
         freezeTableName: true,
