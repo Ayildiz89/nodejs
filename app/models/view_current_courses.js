7f197bc282e69197b662bb738bc2ce5df7a6f7b4
eozbay <eozbayirtibat@yahoo.com>
2020-02-28 18:00:55 +0100

    viewler aciliyor

---
+++ app/models/view_current_courses.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('current_classes', {
+		id: {
+			type: DataTypes.INTEGER(10).UNSIGNED,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		company_id: {
+			type: DataTypes.INTEGER(10),
+			allowNull: false
+		},
+		name: {
+			type: DataTypes.STRING(60),
+			allowNull: false
+		},
+		description: {
+			type: DataTypes.STRING(100),
+			allowNull: false
+		},
+		created_at: {
+			type: DataTypes.DATE,
+			allowNull: false
+		},
+		updated_at: {
+			type: DataTypes.DATE,
+			allowNull: false
+		},
+	}, {
+		tableName: 'current_classes',
+		timestamps: false
+	});
+};
